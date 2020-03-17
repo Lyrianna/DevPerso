ec9f32afdbb5f666f5c7e00666acf77d843438fe
Lyrianna <lambrinephelie@hotmail.fr>
2020-03-17 19:28:34 +0000

    dossiers projets mods

---
+++ KeepInMind/app/src/main/java/com/example/keepinmind/AddNotes.java
@@ -0,0 +1,14 @@
+package com.example.keepinmind;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddNotes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_notes);
+    }
+}
